c7b6e5121d6670da4d14f4f4bdb4345c8a00ee98
Leila Verhaegen <leila@levit.be>
2023-08-23 10:54:18 +0200

    :wip: creating widget for moodtracker

--- sample/models.py
+++ sample/models.py
@@ -1,5 +1,24 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
+
+FEELINGS_CHOICES = [
+    ('ha', 'Happy'),
+    ('jo', 'Joyful'),
+    ('co', 'Content'),
+    ('re', 'Relaxed'),
+    ('lo', 'Loved'),
+    ('va', 'Valued'),
+]
+
+CARING_CHOICES = [
+    ('wa', 'Walking'),
+    ('me', 'Meditate'),
+    ('ba', 'Bath'),
+    ('co', 'Cook'),
+    ('ex', 'Exercise'),
+]
 
 
 class Category(models.Model):
@@ -49,3 +68,26 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.description
+
+
+class Feeling(models.Model):
+    feels = models.CharField(max_length=2, choices=FEELINGS_CHOICES, default=None)
+
+    def __str__(self):
+        return self.feels
+
+
+class Caring(models.Model):
+    cares = models.CharField(max_length=2, choices=CARING_CHOICES, default=None)
+
+    def __str__(self):
+        return self.cares
+
+
+class MoodTracker(models.Model):
+    date = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
+    emote = models.FileField(upload_to=None)
+    feeling = models.ForeignKey(Feeling, blank=True, null=True, on_delete=models.CASCADE, default='ha')
+    care = models.ForeignKey(Caring, blank=True, null=True, on_delete=models.CASCADE, default='co')
