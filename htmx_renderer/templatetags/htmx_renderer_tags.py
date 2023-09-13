80f2f2490d264508b94a7d3afa0378c1fa69f69c
laura <laura@levit.be>
2023-09-13 09:58:15 +0200

    :bug: fix fieldset

--- htmx_renderer/templatetags/htmx_renderer_tags.py
+++ htmx_renderer/templatetags/htmx_renderer_tags.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from urllib.parse import quote_plus
 
 from django.apps import apps
@@ -54,31 +55,63 @@ def qs(context, *ignore):
 
 @register.inclusion_tag('fields/_include_field.html', takes_context=True)
 def include_field_widget(context, is_editable=False, is_filter=False, **kwargs):
+    import pprint
+    print(type(context))
+    print('**********************')
+    new_context = dict({
+        k: context.get(k, None)
+        for k in ('record', 'meta', 'request', 'serializer', 'view')
+    })
     if 'field_name' in kwargs:
         field_name = kwargs['field_name']
-        context['field_name'] = field_name
+        new_context['field_name'] = field_name
+        print('field_name from kwargs')
     elif 'field_name' in context:
         field_name = context['field_name']
+        new_context['field_name'] = field_name
+        print('field_name from context')
     else:
         raise Exception('please provide a field name')
 
-    context['field'] = _field_by_name(context['meta']['fields'], field_name)
+    try:
+        print(f'trying to extract {field_name} from {[f.get("name", "") for f in context["meta"]["fields"]]}')
+        metadata = deepcopy(_field_by_name(context['meta']['fields'], field_name))
+    except IndexError:
+        print(f'could not find {field_name}')
+        # Unnamed field, probably a fieldset or tabset
+        metadata = {}
 
-    if 'metadata' in context:
-        context['field'].update(context['metadata'])
-    if 'metadata' in kwargs:
-        context['field'].update(kwargs['metadata'])
+    print('metadata', metadata)
+
+    if 'field' in kwargs:
+        metadata.update(kwargs['field'])
+    elif 'field' in context:
+        metadata.update(deepcopy(context['field']))
+
+    print('-')
+
+    if 'class' not in metadata:
+        metadata['class'] = kwargs.get('class', '')
+    if 'showLabel' in kwargs:
+        metadata['showLabel'] = kwargs['showLabel']
+    elif 'showLabel' not in metadata:
+        metadata['showLabel'] = True
+    new_context['field'] = metadata
+    print('--')
+    pprint.pprint(new_context['field'])
 
     try:
-        template_name = f'fields/{context["field"]["widget"]}-widget.html'
+        template_name = f'fields/{metadata["widget"]}-widget.html'
         get_template(template_name)
+        print(f'rendering {template_name}')
     except TemplateDoesNotExist:
         template_name = 'fields/_default-widget.html'
+        print('rendring default widget')
 
-    context['to_include'] = template_name
-    context['is_editable'] = is_editable or field_name in context['meta']['list_editable']
-    context['is_filter'] = is_filter
-    return context
+    new_context['to_include'] = template_name
+    new_context['is_editable'] = is_editable or field_name in context['meta']['list_editable']
+    new_context['is_filter'] = is_filter
+    return new_context
 
 
 @register.filter
@@ -154,10 +187,11 @@ def comp_id(active_filter, value):
 
 
 @register.inclusion_tag('htmx/include.html', takes_context=True)
-def include_with_context_as_record(context, template_name):
-    context['record'] = context
+def include_with_context_as_record(context, template_name, **kwargs):
+    context['record'] = context['serializer'].data
     # context['path'] = path
     context['htmx'] = True
+    context.update(kwargs)
     return _render_partial_template(context, template_name)
 
 
